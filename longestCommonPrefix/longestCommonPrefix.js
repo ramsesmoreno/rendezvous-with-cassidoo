b51a8556a0f293beded5c20cd0311a3cbd4aeb7a
Ramses <ramses@cuatromedios.com>
2025-05-05 11:16:43 -0600

    longestCommonPrefix

---
+++ longestCommonPrefix/longestCommonPrefix.js
@@ -0,0 +1,20 @@
+function longestCommonPrefix(strings) {
+  // Create a list of prefixes
+  const prefixes = strings.reduce((acc, string) => {
+    for (let c= 0; c < string.length; c++) {
+      const prefix = string.substring(0, c+1)
+      if (acc[prefix]) acc[prefix]++
+      else acc[prefix] = 1
+    }
+    return acc
+  }, {})
+  const longestPrefix = Object.entries(prefixes).reduce((acc, [prefix, count]) => {
+    if ((prefix.length > acc.prefix.length && count > 1) || (prefix.length === acc.prefix.length && count > acc.count)) return { prefix, count }
+    return acc
+  }, { prefix: '', count: 0 })
+  return longestPrefix.prefix
+}
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["dog","racecar","car"]))
+console.log(longestCommonPrefix(["interstellar","internet","internal","interval"]))
+
